9f581a3a7103e7723a2d81611efa5bee01467b03
MohammedMansour <mohammed.mansour4009@gmail.com>
2023-06-22 12:25:43 +0300

    Implement WebView functionality with navigation controls - Add central "Open Browser" button on main activity - Create new activity (WebActivity) to host WebView and navigation controls - Implement functionality for Back, Forward, Refresh and Close buttons - Set WebView to load a specific URL on startup

--- app/src/main/java/com/assignment/tamatem/MainActivity.kt
+++ app/src/main/java/com/assignment/tamatem/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.assignment.tamatem
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.assignment.tamatem.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.openBrowserButton.setOnClickListener {
+            startActivity(Intent(this@MainActivity, WebViewActivity::class.java))
+        }
     }
 }
